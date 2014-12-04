20c29f70720716001de564cddc477b16edee0aa6
Joshua Richardson <joshrichardson1234@hotmail.com>
2014-12-04 14:06:58 +1100

    Start message bord app

--- js/coltrollers.js
+++ js/coltrollers.js
@@ -1,4 +1,24 @@
 angular.module('hipstrchat')
-  .controller('AppCtrl', function() {
-  console.log('loaded');
-  });
+  // .controller('AppCtrl', function($scope){
+  //   console.log('loaded');
+
+  //   $scope.title = "This is the app ctrl";
+
+  //   $scope.heroes = [
+  //     'Superman', 'Spiderman', 'Stretchguy', 'the Flash'
+  //   ];
+
+  //   $scope.buttonClicked = function () {
+  //     alert('You clicked the button!');
+  //   };
+  // })
+  .controller('HeaderCtrl', function($scope) {
+    $scope.title = "This is the header";
+  })
+  .controller('SidebarCtrl', function($scope) {
+    $scope.title = "This is the sidebar";
+  })
+  .controller('MessagesCtrl', function($scope) {
+    $scope.title = "This is the messages";
+  })
+
